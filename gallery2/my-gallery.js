4300522c7315b4676c8736d3c2fb24c6dcdee607
yashvi180 <yashvi.goradia@gmail.com>
2024-07-21 23:23:46 +0400

    updated style.css for gallery2

--- gallery2/my-gallery.js
+++ gallery2/my-gallery.js
@@ -1,6 +1,9 @@
 new Swiper(".swiper-container",{
     speed:400,
     spaceBetween: 100,
+    effect:"slide",
+    slidesPerView: 3,
+    loop:true,
 navigation: {
 nextEl: ".swiper-button-next",
 prevEl: ".swiper-button-prev"
