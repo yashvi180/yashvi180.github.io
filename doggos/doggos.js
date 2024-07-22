2f232d6129a25f17e77f26b0b333f3fa1f4952f4
yashvi180 <yashvi.goradia@gmail.com>
2024-07-22 10:53:37 +0400

    edited java script and html

---
+++ doggos/doggos.js
@@ -0,0 +1,16 @@
+const BREEDS_URL = "https://dog.ceo/api/breeds/image/random";
+function addDoggo() {
+    fetch(BREEDS_URL)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+        const img =document.createElement('img')
+        img.src=data.message
+        img.alt='cute doggo'
+        document.querySelector('.doggo' ).appendChild(img);
+      })
+}
+document.querySelector('.add-doggo').addEventListener("click",addDoggo)
+
+  
